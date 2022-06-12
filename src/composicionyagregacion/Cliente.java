39810ded3ca77baf73e2ba0889edb82da76504ac
czunigac99 <czunigac@unah.hn>
2022-06-12 14:44:30 -0500

    creando la clase cliente

---
+++ src/composicionyagregacion/Cliente.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package composicionyagregacion;
+
+/**
+ *
+ * @author cris
+ */
+public class Cliente {
+    
+ String NombreCliente;
+ 
+public Cliente(){
+super();
+}    
+
+public Cliente(String NombreCliente){
+this.NombreCliente=NombreCliente;
+}
+public void Set_Cliente(String NombreCliente){
+this.NombreCliente=NombreCliente;
+}    
+public String get_Cliente(){
+return NombreCliente;
+}
+
+}
