bfcb7589a8356fdad408649dc76597c5726cb600
czunigac99 <czunigac@unah.hn>
2022-06-11 21:31:59 -0500

    Inicializando y creando metodos get y set

---
+++ src/composicionyagregacion/Empleado.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package composicionyagregacion;
+
+/**
+ *
+ * @author cris
+ */
+    
+        
+public class Empleado {
+
+private String Nombre;
+private float Sueldo;   
+
+public Empleado(){
+
+super();
+}
+
+public Empleado(String Nombre, float Sueldo){
+this.Nombre=Nombre;
+this.Sueldo=Sueldo;
+
+
+
+}
+
+}
